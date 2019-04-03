1de866b1254581fc4ac9f3257621ef8872adf533
Kevin W <kcwang06@gmail.com>
2019-04-03 21:29:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  dogs= [shiba, lab, golden retriever, pugs]
+  dogs= [shiba, lab, goldenretriever, pugs]
 end
 
 def add_element_to_end_of_array(array, element)
